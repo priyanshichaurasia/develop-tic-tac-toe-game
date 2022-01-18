ea86d0e1ca2bc93ff03fc5da70d7a288fe29103b
priyanshic83@gmail.com <priyanshic83@gmail.com>
2022-01-18 09:20:34 +0530

    commit 2

--- js/script.js
+++ js/script.js
@@ -1,198 +1,3 @@
-// button click handler
-// flag = 1;
-// const b1 = (event) => {
-//   event.preventDefault();
-//   f1();
-//   if (flag == 1) {
-//     document.getElementById("box-1").innerHTML = "X";
-//     document.getElementById("box-1").disabled = true;
-//     flag = 0;
-//   } else {
-//     document.getElementById("box-1").innerHTML = "O";
-//     document.getElementById("box-1").style.color = "blue";
-//     document.getElementById("box-1").disabled = true;
-//     flag = 1;
-//   }
-// };
-// const b2 = (event) => {
-//   event.preventDefault();
-//   f1();
-//   if (flag == 1) {
-//     document.getElementById("box-2").innerHTML = "X";
-//     document.getElementById("box-2").disabled = true;
-//     flag = 0;
-//   } else {
-//     document.getElementById("box-2").innerHTML = "O";
-//     document.getElementById("box-2").style.color = "blue";
-//     document.getElementById("box-2").disabled = true;
-//     flag = 1;
-//   }
-// };
-// const b3 = (event) => {
-//   event.preventDefault();
-//   f1();
-//   if (flag == 1) {
-//     document.getElementById("box-3").innerHTML = "X";
-//     document.getElementById("box-3").disabled = true;
-//     flag = 0;
-//   } else {
-//     document.getElementById("box-3").innerHTML = "O";
-//     document.getElementById("box-3").style.color = "blue";
-//     document.getElementById("box-3").disabled = true;
-//     flag = 1;
-//   }
-// };
-// const b4 = (event) => {
-//   event.preventDefault();
-//   f1();
-//   if (flag == 1) {
-//     document.getElementById("box-4").innerHTML = "X";
-//     document.getElementById("box-4").disabled = true;
-//     flag = 0;
-//   } else {
-//     document.getElementById("box-4").innerHTML = "O";
-//     document.getElementById("box-4").style.color = "blue";
-//     document.getElementById("box-4").disabled = true;
-//     flag = 1;
-//   }
-// };
-// const b5 = (event) => {
-//   event.preventDefault();
-//   f1();
-//   if (flag == 1) {
-//     document.getElementById("box-5").innerHTML = "X";
-//     document.getElementById("box-5").disabled = true;
-//     flag = 0;
-//   } else {
-//     document.getElementById("box-5").innerHTML = "O";
-//     document.getElementById("box-5").style.color = "blue";
-//     document.getElementById("box-5").disabled = true;
-//     flag = 1;
-//   }
-// };
-// const b6 = (event) => {
-//   event.preventDefault();
-//   f1();
-//   if (flag == 1) {
-//     document.getElementById("box-6").innerHTML = "X";
-//     document.getElementById("box-6").disabled = true;
-//     flag = 0;
-//   } else {
-//     document.getElementById("box-6").innerHTML = "O";
-//     document.getElementById("box-6").style.color = "blue";
-//     document.getElementById("box-6").disabled = true;
-//     flag = 1;
-//   }
-// };
-// const b7 = (event) => {
-//   event.preventDefault();
-//   f1();
-//   if (flag == 1) {
-//     document.getElementById("box-7").innerHTML = "X";
-//     document.getElementById("box-7").disabled = true;
-//     flag = 0;
-//   } else {
-//     document.getElementById("box-7").innerHTML = "O";
-//     document.getElementById("box-7").style.color = "blue";
-//     document.getElementById("box-7").disabled = true;
-//     flag = 1;
-//   }
-// };
-// const b8 = (event) => {
-//   event.preventDefault();
-//   f1();
-//   if (flag == 1) {
-//     document.getElementById("box-8").innerHTML = "X";
-//     document.getElementById("box-8").disabled = true;
-//     flag = 0;
-//   } else {
-//     document.getElementById("box-8").innerHTML = "O";
-//     document.getElementById("box-8").style.color = "blue";
-//     document.getElementById("box-8").disabled = true;
-//     flag = 1;
-//   }
-// };
-// const b9 = (event) => {
-//   event.preventDefault();
-//   f1();
-//   if (flag == 1) {
-//     document.getElementById("box-9").innerHTML = "X";
-//     document.getElementById("box-9").disabled = true;
-//     flag = 0;
-//   } else {
-//     document.getElementById("box-9").innerHTML = "O";
-//     document.getElementById("box-9").style.color = "blue";
-//     document.getElementById("box-9").disabled = true;
-//     flag = 1;
-//   }
-// };
-
-//apply event to generate new game state
-
-// game state renderer renders the generated game state
-
-// renders text on button clicked with X or O
-
-// disable the button clicked
-
-// update panel values such as Turn Played By and Moves Left
-// function f1() {
-//   if (flag == 1) {
-//     document.getElementById("next-player").innerHTML = "Turn Played By: X";
-//   } else {
-//     document.getElementById("next-player").innerHTML = "Turn Played By: O";
-//   }
-// }
-
-// reset panel values to default values
-
-// implement logic to get the winner
-// const combos = [
-//   [0, 1, 2],
-//   [3, 4, 5],
-//   [6, 7, 8],
-//   [0, 3, 6],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [0, 4, 8],
-//   [6, 4, 2],
-// ];
-
-// let ply1 = [3][3];
-// let ply2 = [3][3];
-// function checkWinner() {
-//   for (i = 0; i < ply1.length; i++) {
-//     for (j = 1; j < ply2.length; j++) {
-//       if (
-//         (ply1[0] == "1" && ply1[4] == "1" && ply1[8] == "1") ||
-//         (ply1[0] == "1" && ply1[3] == "1" && ply1[6] == "1")
-//       ) {
-//         console.log("player1 win");
-//       }
-//     }
-//   }
-// }
-
-// announce winner
-
-// REPLAY-MODE :: replay-game-button-clicked->fetches events recorded->apply event->generates new game state->render game state
-
-// reset game to start a new
-
-// bind events to clickable buttons
-// button click handler
-
-//apply event to generate new game state
-
-// game state renderer renders the generated game state
-
-// renders text on button clicked with X or O
-
-// disable the button clicked
-
-// update panel values such as Turn Played By and Moves Left
-
-// reset panel values to default values
 const fnreset = (event) => {
   event.preventDefault();
   location.reload();
@@ -206,61 +11,118 @@ const fnreset = (event) => {
   document.getElementById("box-8").value = "";
   document.getElementById("box-9").value = "";
 };
-
-// implement logic to get the winner
-
-// announce winner
-
-// REPLAY-MODE :: replay-game-button-clicked->fetches events recorded->apply event->generates new game state->render game state
-
-// reset game to start a new
-
-// bind events to clickable buttons
-
+​
 //matrix method
 var turn = 1;
 var win = -1;
 var clickcounter = 0;
-var matrix = [[-1 - 1 - 1], [-1, -1, -1], [-1, -1, -1]];
-
-const checkwin = (e, row, col) => {
-  if (e.innerHTML != "") return;
-  if (win != -1) return;
-
+//var matrix = [[-1 - 1 - 1], [-1, -1, -1], [-1, -1, -1]];
+var matrix = [[-1, - 1, - 1], [-1, -1, -1], [-1, -1, -1]];
+​
+const checkwin = (e, row, col) => { 
+  document.getElementById(e.target.id).setAttribute('disabled',true);
+  //if (e.innerHTML != "") return;
+  //if (win != -1) return;
+​
   clickcounter++;
-
+console.log(matrix);
   matrix[row][col] = turn;
   if (turn == 1) {
-    e.innerHTML = "X";
+    //e.innerHTML = "X";
+    document.getElementById(e.target.id).innerHTML='X';
     document.getElementById("next-player").innerHTML = "Turn Played By: X";
     turn = 2;
   } else {
-    e.innerHTML = "O";
+    //e.innerHTML = "O";
+    document.getElementById(e.target.id).innerHTML='O';
     document.getElementById("next-player").innerHTML = "Turn Played By: O";
     turn = 1;
   }
-  for (var i = 0; i < 3; i++) {
-    //row check
-    if (matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2])
-      win = matrix[i][0];
+​
+  let p1flag=false;
+  let p2flag=false;
+​
+   
+  for (var i = 0; i < 3; i++) { // check for p1
+    if(matrix[i][0]==1 && matrix[i][1]==1 && matrix[i][2]==1)  {// p1 rowcheck
+      p1flag=true;
+      win=1;
+      console.log('p1 a ' +p1flag);
+    }
+    else if (matrix[0][i]==1 && matrix[1][i]==1 && matrix[2][i]==1) { // p1 col check
+      p1flag=true;
+      win=1;
+      console.log('p1 b ' +p1flag);
+    }     
+    if(matrix[i][0]==2 && matrix[i][1]==2 && matrix[i][2]==2)  {// p2 row check
+      p2flag=true;
+      win=2;
+      console.log('p2 a ' +p2flag);
+    }
+    else if (matrix[0][i]==2 && matrix[1][i]==2 && matrix[2][i]==2) { // p2 col check
+      p2flag=true;
+      win=2;
+      console.log('p2 b ' +p2flag);
+    }    
+  }
+  if (matrix[0][0]==1 && matrix[1][1]==1 && matrix[2][2]==1) {     // p1 diag check
+    p1flag=true;
+    win=1;
+    console.log('p1 c ' +p1flag);
+  }
+  else if (matrix[0][0]==2 && matrix[1][1]==2 && matrix[2][2]==2) {     // p2 diag check
+    p2flag=true;
+    win=2;
+    console.log('p2 c ' +p2flag);
+  }
+ 
+    // for (var i = 0; i < 3; i++) { // check for p2
+    //   console.log('p2 loop' +matrix[i][0] + matrix[i][1] +matrix[i][2]);
+    //   if(matrix[i][0]==matrix[i][1]==matrix[i][2]==2)  {
+    //     p2flag=true;
+    //     win=2;
+    //     console.log('p2 a ' +p2flag);
+    //   }
+    //   else if (matrix[0][i]==matrix[1][i]==matrix[2][i]==2) {
+    //     p2flag=true;
+    //     win=2;
+    //     console.log('p2 b ' +p2flag);
+    //   } 
+    //   else if (matrix[0][0]==matrix[1][1]==matrix[2][2]==2) {    
+    //     p2flag=true;
+    //     win=2;
+    //     console.log('p2 c ' +p2flag);
+    //   }
+    // }
+  
+​
+//    if (matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2])
+  //    win = matrix[i][0];
     //col check
     // if (matrix[0][i] == matrix[1][i] && matrix[1][1] == matrix[2][i])
     //   win = matrix[0][i];
-  }
-  for (var i = 0; i < 3; i++) {
-    //col check
-    if (matrix[0][i] == matrix[1][i] && matrix[1][1] == matrix[2][i])
-      win = matrix[0][i];
-  }
-
+     
+ //}
+  // for (var i = 0; i < 3; i++) {
+  //   //col check
+  //   if (matrix[0][i] == matrix[1][i] && matrix[1][1] == matrix[2][i])
+  //     win = matrix[0][i];
+  // }
+​
   //diagonal
-  if ((matrix[0][0] == matrix[1][1]) == matrix[2][2]) win = matrix[1][1];
-  if ((matrix[0][2] == matrix[1][1]) == matrix[2][0]) win = matrix[1][1];
-
-  if (win != -1) {
+  // if ((matrix[0][0] == matrix[1][1]) == matrix[2][2]) win = matrix[1][1];
+  // if ((matrix[0][2] == matrix[1][1]) == matrix[2][0]) win = matrix[1][1];
+​
+  // if (win != -1) {
+  //   alert("player" + win + "has won match");
+  //   location.reload();
+  // }
+  if(p1flag==true || p2flag==true){
     alert("player" + win + "has won match");
+    location.reload();
   }
   if (clickcounter == 9 && win == -1) {
     alert(" Match is draw");
+    location.reload();
   }
 };
